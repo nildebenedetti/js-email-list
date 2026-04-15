a2c71bb3710dca95aad20ac6367c71dfb932db93
“Nilde <nilde.benedetti@gmail.com>
2026-04-15 15:57:40 +0200

    add: variabile con random mail - script.js

--- script/script.js
+++ script/script.js
@@ -9,17 +9,23 @@ const API_RANDOM_EMAIL = 'https://flynn.boolean.careers/exercises/api/random/mai
 // poi da data devo stampare del json con proprieta' .json
 
 
-// mi creo una bella funzione per stampare le mail a comando
-function getEmail() {
-
 fetch(API_RANDOM_EMAIL) // vado a cercare le API e restituisco una promessa (quest e'l promessa)
     .then ((risposta) => { // QUANDO la promessa si avvera, ALLORA
         return risposta.json // mi salva e restituisce il valore risposta in formato json per la prossima promessa (appliciamo il metodo json)
     })
     .then ((jsonData) => { // QUANDO anche la promessa di trasformazione del dato sara mantenuta, ALLORA
-        console.log(jsonData); // STAMPA l'oggetto/ array DI OGGETTI in console
+        console.log('chiamata driin'); // checkpoint se la chiamata e'andata
+        const randomEmail = jsonData.response;
+          // mi restituirai il valore corrispondente alla chiave response dentro oggetto json
     });
-};
+
+    
+
+
+
+
+
+
 
 
 
