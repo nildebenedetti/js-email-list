3b9eb561b5b20d083084dc7e7f78822a36f7be2a
“Nilde <nilde.benedetti@gmail.com>
2026-04-15 15:33:55 +0200

    fetch: api per generare indirizzi mail con catena di promesse - script.js

--- script/script.js
+++ script/script.js
@@ -3,4 +3,14 @@
 // assegno API url a variabile
 const API_RANDOM_EMAIL = 'https://flynn.boolean.careers/exercises/api/random/mail;'
 
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// per farlo devo farmi il fetch e rtestituire data
+// poi da data devo stampare del json con proprieta' .json
 
+fetch(API_RANDOM_EMAIL) // vado a cercare le API e restituisco una promessa (quest e'l promessa)
+    .then ((risposta) => { // QUANDO la promessa si avvera, ALLORA
+        return risposta.json // mi salva e restituisce il valore risposta in formato json per la prossima promessa (appliciamo il metodo json)
+    })
+    .then (promise (jsonData) => { // QUANDO anche la promessa di trasformazione del dato sara mantenuta, ALLORA
+        console.log(jsonData); // STAMPA l'oggetto/ array DI OGGETTI in console
+    });
