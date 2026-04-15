cfec43c861959145f5010b492057f37f7eceeef7
“Nilde <nilde.benedetti@gmail.com>
2026-04-15 16:48:08 +0200

    add: itero 10 volte la mia funzione

--- script/script.js
+++ script/script.js
@@ -13,6 +13,9 @@ const divEmail = document.querySelector('.stampa-qui')
 // per farlo devo farmi il fetch e rtestituire data
 // poi da data devo stampare del json con proprieta' .json
 
+//per iterarlo 10 volte devo inserirlo in un ciclo for
+
+for ( let i = 0; i < 10; i++) {
 
 fetch(API_RANDOM_EMAIL) // vado a cercare le API e restituisco una promessa (quest e'l promessa)
     .then ((risposta) => { // QUANDO la promessa si avvera, ALLORA
@@ -21,10 +24,10 @@ fetch(API_RANDOM_EMAIL) // vado a cercare le API e restituisco una promessa (que
     .then ((jsonData) => { // QUANDO anche la promessa di trasformazione del dato sara mantenuta, ALLORA
         console.log(jsonData.response); // checkpoint se la chiamata e'andata
         const randomEmail = jsonData.response;
-        divEmail.innerHTML = `Eccoti una mail ${randomEmail}`;
+        divEmail.innerHTML += `<li>${randomEmail}</>`;
     });
 
-    
+};
 
  // async await   approfondire 
 
