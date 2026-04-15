6a21f5831487b33f351fe3e71b1df6bae0bce296
“Nilde <nilde.benedetti@gmail.com>
2026-04-15 15:47:04 +0200

    inserisco la chiamata in una funzione

--- script/script.js
+++ script/script.js
@@ -8,6 +8,10 @@ const API_RANDOM_EMAIL = 'https://flynn.boolean.careers/exercises/api/random/mai
 // per farlo devo farmi il fetch e rtestituire data
 // poi da data devo stampare del json con proprieta' .json
 
+
+// mi creo una bella funzione per stampare le mail a comando
+function getEmail() {
+
 fetch(API_RANDOM_EMAIL) // vado a cercare le API e restituisco una promessa (quest e'l promessa)
     .then ((risposta) => { // QUANDO la promessa si avvera, ALLORA
         return risposta.json // mi salva e restituisce il valore risposta in formato json per la prossima promessa (appliciamo il metodo json)
@@ -15,3 +19,7 @@ fetch(API_RANDOM_EMAIL) // vado a cercare le API e restituisco una promessa (que
     .then ((jsonData) => { // QUANDO anche la promessa di trasformazione del dato sara mantenuta, ALLORA
         console.log(jsonData); // STAMPA l'oggetto/ array DI OGGETTI in console
     });
+};
+
+
+
